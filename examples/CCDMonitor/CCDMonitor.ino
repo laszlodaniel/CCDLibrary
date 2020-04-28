e21d218aa0cccc727f148937c1c513dae9328d12
László Dániel <laszlodaniel@gmail.com>
2020-04-28 18:44:46 +0200

    Update examples
    Removed unnecessary whitespaces.

--- examples/CCDMonitor/CCDMonitor.ino
+++ examples/CCDMonitor/CCDMonitor.ino
@@ -30,10 +30,10 @@ void setup()
     #if defined (ChryslerCCDSCIScanner)
         TCCR1A = 0;                        // clear register
         TCCR1B = 0;                        // clear register
-        TCNT1  = 0;                        // clear counter
-        DDRB   |= (1<<DDB5);               // set OC1A/PB5 as output
+        TCNT1 = 0;                         // clear counter
+        DDRB |= (1<<DDB5);                 // set OC1A/PB5 as output
         TCCR1A |= (1<<COM1A0);             // toggle OC1A on compare match
-        OCR1A  = 7;                        // top value for counter, toggle after counting to 8 (0->7) = 2 MHz interrupt ( = 16 MHz clock frequency / 8)
+        OCR1A = 7;                         // top value for counter, toggle after counting to 8 (0->7) = 2 MHz interrupt ( = 16 MHz clock frequency / 8)
         TCCR1B |= (1<<WGM12) | (1<<CS10);  // CTC mode, prescaler clock/1 (no prescaler)
     #endif
     
